6b0e49a54522214e428319a3c6769f653ea49d5f
Davide Nastri <d.nastri@gmail.com>
2020-04-05 22:33:23 +0200

    feat: Implemented Setter Injected Controller and test

---
+++ src/main/java/it/davidenastri/javaspringdependencyinjectionexamples/controllers/SetterInjectedController.java
@@ -0,0 +1,20 @@
+package it.davidenastri.javaspringdependencyinjectionexamples.controllers;
+
+import it.davidenastri.javaspringdependencyinjectionexamples.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/it/davidenastri/javaspringdependencyinjectionexamples/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package it.davidenastri.javaspringdependencyinjectionexamples.controllers;
+
+import it.davidenastri.javaspringdependencyinjectionexamples.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+    // Method used by many in the community
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
