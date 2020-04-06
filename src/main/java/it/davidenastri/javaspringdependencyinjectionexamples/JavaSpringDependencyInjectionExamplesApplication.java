eac09c092057a0d3916ccefeff8cccc3fdfff4a7
Davide Nastri <d.nastri@gmail.com>
2020-04-06 22:40:45 +0200

    feat: Added an Internationalization controller and two localized services that can be selected via the active Profile

--- src/main/java/it/davidenastri/javaspringdependencyinjectionexamples/JavaSpringDependencyInjectionExamplesApplication.java
+++ src/main/java/it/davidenastri/javaspringdependencyinjectionexamples/JavaSpringDependencyInjectionExamplesApplication.java
@@ -1,9 +1,6 @@
 package it.davidenastri.javaspringdependencyinjectionexamples;
 
-import it.davidenastri.javaspringdependencyinjectionexamples.controllers.ConstructorInjectedController;
-import it.davidenastri.javaspringdependencyinjectionexamples.controllers.MyController;
-import it.davidenastri.javaspringdependencyinjectionexamples.controllers.PropertyInjectedController;
-import it.davidenastri.javaspringdependencyinjectionexamples.controllers.SetterInjectedController;
+import it.davidenastri.javaspringdependencyinjectionexamples.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,9 +11,13 @@ public class JavaSpringDependencyInjectionExamplesApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = SpringApplication.run(JavaSpringDependencyInjectionExamplesApplication.class, args);
-		MyController myController = (MyController) ctx.getBean("myController");
 
-		System.out.println("\n----- Primary Bean");
+		System.out.println("\n----- Active profile");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
+
+		MyController myController = (MyController) ctx.getBean("myController");
+		System.out.println("\n----- Primary bean");
 		System.out.println(myController.sayHello());
 
 		System.out.println("\n----- Property injected dependency");
