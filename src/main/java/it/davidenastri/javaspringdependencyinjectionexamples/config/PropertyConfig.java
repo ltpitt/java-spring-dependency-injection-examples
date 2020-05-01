fc1ef1f7b225dd61872a21b3f5af3fea4b489b8d
Davide Nastri <d.nastri@gmail.com>
2020-05-01 21:19:05 +0200

    feat: Read properties from the Environment variables

--- src/main/java/it/davidenastri/javaspringdependencyinjectionexamples/config/PropertyConfig.java
+++ src/main/java/it/davidenastri/javaspringdependencyinjectionexamples/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package it.davidenastri.javaspringdependencyinjectionexamples.config;
 
 import it.davidenastri.javaspringdependencyinjectionexamples.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${javaspringdependencyinjectionexamples.user}")
     String user;
@@ -29,7 +34,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setDbUrl(dburl);
         return fakeDataSource;
